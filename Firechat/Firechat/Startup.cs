1df4920eb39b95b9c392be3267dcf5c78e5c5690
altairObando <pxnfilo@gmail.com>
2019-12-25 22:53:52 -0600

    Initial Commit

---
+++ Firechat/Firechat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Firechat.Startup))]
+namespace Firechat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
